f9362ca0f033ea61451dd5fc7ecfa6e0f784f9ed
= <=>
2025-10-09 11:23:01 +0530

    initial commit

---
+++ app/_layout.jsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+import "./global.css";
+import { Text, View } from "react-native";
+
+const RootLayout = () => {
+  return (
+      <Stack />
+  );
+};
+
+export default RootLayout;
