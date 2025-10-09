45ca4782e8898063c69243d9e7b5e544d60f5c4a
= <=>
2025-10-09 12:08:26 +0530

    home page done

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
 import "./global.css";
 import { Text, View } from "react-native";
+import { PaperProvider } from "react-native-paper";
 
 const RootLayout = () => {
   return (
+    <PaperProvider>
       <Stack />
+    </PaperProvider>
   );
 };
 
--- app/main.jsx
+++ app/main.jsx
@@ -1,11 +1,39 @@
-import { Text, View } from "react-native";
- 
-export default function App() {
+import { Text, View, FlatList, TouchableOpacity } from "react-native";
+import { Link } from "expo-router";
+import { FAB } from "react-native-paper";
+import { useRouter } from "expo-router";
+
+export default function Main() {
+  const router = useRouter();
+  const people = [
+    { id: "1", name: "Ava", quizzes: 3 },
+    { id: "2", name: "Noah", quizzes: 1 },
+    { id: "3", name: "Mia", quizzes: 5 },
+  ];
+
   return (
-    <View className="flex-1 items-center justify-center bg-blue">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+    <View className="flex-1 bg-white p-4">
+      <FlatList
+        data={people}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View className="p-4 border-b border-gray-200">
+            <Text className="text-lg font-semibold">{item.name}</Text>
+          </View>
+        )}
+      />
+
+      <FAB
+        icon="plus"
+        style={{
+          position: "absolute",
+          right: 16,
+          bottom: 16,
+          backgroundColor: "#22c55e", // green
+        }}
+        color="white"
+        onPress={() => router.push("/addNew")} // navigate to page
+      />
     </View>
   );
-} 
+}
